4445897512d12be8f62f139a23ca18492bf71142
elektrowolle <internetbewohner@gmail.com>
2014-05-24 16:20:05 +0200

    removed all compiler errors

--- BEN.cpp
+++ BEN.cpp
@@ -1,41 +1,51 @@
 #include "BEN.h"
 
-BEN::BEN ( int pin, void (*userFunc)(void) ) {
-	PIN     = 0       ;
-	intFunc = userFunc;
-	BENClass.attach(PIN, intFunc);
+BEN::BEN (int pin, int address, void (*userFunc)(void)) {
+	this->ADDRESS = address;
+	this->PIN     = pin;
+	this->intFunc = userFunc;
+
+	BENClass::attach(this->PIN, this->intFunc);
 }
 
-BENClass::attach ( int pin, void (*userFunc)(void) ) {
-	BENCLass.init();
-	
-	intFunc[pin] = userFunc;
-
+bool BEN::send (int address, char *message[] ) {
+	return false;
 }
 
-BENClass::init () {
-	if(!BENClass.ENABLED) {
+void BENClass::attach (int pin, void (*userFunc)(void)) {
+	BENClass::init();
+	BENClass::intFunc[pin] = (uintptr_t) &userFunc;
+}
+
+void BENClass::init () {
+	if(!BENClass::ENABLED) {
 		cli();
 
 		if(MCUSR & _BV(WDRF)){            // If a reset was caused by the Watchdog Timer...
-    		MCUSR &= ~_BV(WDRF);                 // Clear the WDT reset flag
-	    	WDTCSR |= (_BV(WDCE) | _BV(WDE));   // Enable the WD Change Bit
-	    	WDTCSR = 0x00;                      // Disable the WDT
+    		MCUSR  &= ~_BV(WDRF);                 // Clear the WDT reset flag
+	    	WDTCSR |= (_BV(WDCE) | _BV(WDE));     // Enable the WD Change Bit
+	    	WDTCSR  = 0x00;                       // Disable the WDT
 		} else 
 		{
 			// Set up Watch Dog Timer for Inactivity
-			WDTCSR |= (_BV(WDCE) | _BV(WDE));   // Enable the WD Change Bit
-			WDTCSR =   _BV(WDIE) |              // Enable WDT Interrupt
-			           _BV(WDP2) | _BV(WDP1);   // Set Timeout to ~1 seconds
+			WDTCSR |= (_BV(WDCE) | _BV(WDE));       // Enable the WD Change Bit
+			WDTCSR  =   _BV(WDIE);// |              // Enable WDT Interrupt
+			           //_BV(WDP2) | _BV(WDP1);     // Set Timeout to .16 seconds
 			sei();
 		}
 	}
 }
 
+void BENClass::trigger(int pin) {
+	void (*_funcPtr) (void) = (void (*)(void)) intFunc[pin];
+	_funcPtr();
+}
+
+
 ISR (WDT_vect) {
-	sleep_disable();        // Disable Sleep on Wakeup
+	//sleep_disable();        // Disable Sleep on Wakeup
 
- 	BENClass.listen();
+ 	BENClass::listen();
 
- 	sleep_enable();        // Enable Sleep Mode
+ 	//sleep_enable();         // Enable Sleep Mode
 }
